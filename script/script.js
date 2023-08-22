3394a1bd3d4a20c32d354e31e42ad0793a44a45a
webdevtamim <tamimahmed12.bd@gmail.com>
2023-08-22 13:49:50 +0600

    sixth commit

--- script/script.js
+++ script/script.js
@@ -1,18 +1,398 @@
-// total
-const totalPrice = document.getElementById('total-price').innerText
-const totalPriceValue = parseFloat(totalPrice)
-const discount = totalPriceValue * 0.2
-const total = totalPriceValue - discount
-if(totalPriceValue >= 200) {
+// product quantity
+document.getElementById('prod1').addEventListener('click', function(){
+    const prodPriceValue = document.getElementById('quantity1')
+    const prodPriceString = prodPriceValue.innerText
+    const prodPrice = parseFloat(prodPriceString)
+
+    const totalElement = document.getElementById('total-price')
+    const previousTotalString = totalElement.innerText
+    const previousTotal = parseFloat(previousTotalString)
+
+    const currentTotalQuantity = previousTotal + prodPrice
+    const currentTotal = currentTotalQuantity.toFixed(2)
+
+    totalElement.innerText = currentTotal
+
+    if(currentTotal >= 200){
+        document.getElementById('apply-btn').addEventListener('click', function(){
+            const promoInput = document.getElementById('remove-outline')
+            const promoInputValue = promoInput.value
+            if(promoInputValue === 'SELL200'){
+                document.getElementById('apply-btn').addEventListener('click', function(){
+                    const discountValue = currentTotal * 0.2
+                    const discount = discountValue.toFixed(2)
+                    const totalValue = currentTotal - discount
+                    const total = totalValue.toFixed(2)
+                    document.getElementById('discount').innerText = discount
+                    document.getElementById('total').innerText = total
+                })
+            }
+        })
+        document.getElementById('apply-btn').disabled = false
+        document.getElementById('apply-btn').style.backgroundColor = '#E527B2'
+    }
+
+    const discount = 0
+    const totalValue = currentTotal - discount
+    const total = totalValue.toFixed(2)
     document.getElementById('discount').innerText = discount
     document.getElementById('total').innerText = total
-}
-else {
-    document.getElementById('total').innerText = totalPriceValue
-}
 
-
-// product quantity 
-document.getElementById('prod1').addEventListener('click' function(){
-    const prod1 = totalPriceValue
+    if(total > 0){
+        document.getElementById('purchase-btn').disabled = false
+        document.getElementById('purchase-btn').style.backgroundColor = '#E527B2'
+    }
 })
+document.getElementById('prod2').addEventListener('click', function(){
+    const prodPriceValue = document.getElementById('quantity1')
+    const prodPriceString = prodPriceValue.innerText
+    const prodPrice = parseFloat(prodPriceString)
+
+    const totalElement = document.getElementById('total-price')
+    const previousTotalString = totalElement.innerText
+    const previousTotal = parseFloat(previousTotalString)
+
+    const currentTotalQuantity = previousTotal + prodPrice
+    const currentTotal = currentTotalQuantity.toFixed(2)
+
+    totalElement.innerText = currentTotal
+
+    if(currentTotal >= 200){
+        document.getElementById('apply-btn').addEventListener('click', function(){
+            const promoInput = document.getElementById('remove-outline')
+            const promoInputValue = promoInput.value
+            if(promoInputValue === 'SELL200'){
+                document.getElementById('apply-btn').addEventListener('click', function(){
+                    const discountValue = currentTotal * 0.2
+                    const discount = discountValue.toFixed(2)
+                    const totalValue = currentTotal - discount
+                    const total = totalValue.toFixed(2)
+                    document.getElementById('discount').innerText = discount
+                    document.getElementById('total').innerText = total
+                })
+            }
+        })
+        document.getElementById('apply-btn').disabled = false
+        document.getElementById('apply-btn').style.backgroundColor = '#E527B2'
+    }
+
+    const discount = 0
+    const totalValue = currentTotal - discount
+    const total = totalValue.toFixed(2)
+    document.getElementById('discount').innerText = discount
+    document.getElementById('total').innerText = total
+
+    if(total > 0){
+        document.getElementById('purchase-btn').disabled = false
+        document.getElementById('purchase-btn').style.backgroundColor = '#E527B2'
+    }
+})
+document.getElementById('prod3').addEventListener('click', function(){
+    const prodPriceValue = document.getElementById('quantity1')
+    const prodPriceString = prodPriceValue.innerText
+    const prodPrice = parseFloat(prodPriceString)
+
+    const totalElement = document.getElementById('total-price')
+    const previousTotalString = totalElement.innerText
+    const previousTotal = parseFloat(previousTotalString)
+
+    const currentTotalQuantity = previousTotal + prodPrice
+    const currentTotal = currentTotalQuantity.toFixed(2)
+
+    totalElement.innerText = currentTotal
+
+    if(currentTotal >= 200){
+        document.getElementById('apply-btn').addEventListener('click', function(){
+            const promoInput = document.getElementById('remove-outline')
+            const promoInputValue = promoInput.value
+            if(promoInputValue === 'SELL200'){
+                document.getElementById('apply-btn').addEventListener('click', function(){
+                    const discountValue = currentTotal * 0.2
+                    const discount = discountValue.toFixed(2)
+                    const totalValue = currentTotal - discount
+                    const total = totalValue.toFixed(2)
+                    document.getElementById('discount').innerText = discount
+                    document.getElementById('total').innerText = total
+                })
+            }
+        })
+        document.getElementById('apply-btn').disabled = false
+        document.getElementById('apply-btn').style.backgroundColor = '#E527B2'
+    }
+
+    const discount = 0
+    const totalValue = currentTotal - discount
+    const total = totalValue.toFixed(2)
+    document.getElementById('discount').innerText = discount
+    document.getElementById('total').innerText = total
+
+    if(total > 0){
+        document.getElementById('purchase-btn').disabled = false
+        document.getElementById('purchase-btn').style.backgroundColor = '#E527B2'
+    }
+})
+document.getElementById('prod4').addEventListener('click', function(){
+    const prodPriceValue = document.getElementById('quantity1')
+    const prodPriceString = prodPriceValue.innerText
+    const prodPrice = parseFloat(prodPriceString)
+
+    const totalElement = document.getElementById('total-price')
+    const previousTotalString = totalElement.innerText
+    const previousTotal = parseFloat(previousTotalString)
+
+    const currentTotalQuantity = previousTotal + prodPrice
+    const currentTotal = currentTotalQuantity.toFixed(2)
+
+    totalElement.innerText = currentTotal
+
+    if(currentTotal >= 200){
+        document.getElementById('apply-btn').addEventListener('click', function(){
+            const promoInput = document.getElementById('remove-outline')
+            const promoInputValue = promoInput.value
+            if(promoInputValue === 'SELL200'){
+                document.getElementById('apply-btn').addEventListener('click', function(){
+                    const discountValue = currentTotal * 0.2
+                    const discount = discountValue.toFixed(2)
+                    const totalValue = currentTotal - discount
+                    const total = totalValue.toFixed(2)
+                    document.getElementById('discount').innerText = discount
+                    document.getElementById('total').innerText = total
+                })
+            }
+        })
+        document.getElementById('apply-btn').disabled = false
+        document.getElementById('apply-btn').style.backgroundColor = '#E527B2'
+    }
+
+    const discount = 0
+    const totalValue = currentTotal - discount
+    const total = totalValue.toFixed(2)
+    document.getElementById('discount').innerText = discount
+    document.getElementById('total').innerText = total
+
+    if(total > 0){
+        document.getElementById('purchase-btn').disabled = false
+        document.getElementById('purchase-btn').style.backgroundColor = '#E527B2'
+    }
+})
+document.getElementById('prod5').addEventListener('click', function(){
+    const prodPriceValue = document.getElementById('quantity1')
+    const prodPriceString = prodPriceValue.innerText
+    const prodPrice = parseFloat(prodPriceString)
+
+    const totalElement = document.getElementById('total-price')
+    const previousTotalString = totalElement.innerText
+    const previousTotal = parseFloat(previousTotalString)
+
+    const currentTotalQuantity = previousTotal + prodPrice
+    const currentTotal = currentTotalQuantity.toFixed(2)
+
+    totalElement.innerText = currentTotal
+
+    if(currentTotal >= 200){
+        document.getElementById('apply-btn').addEventListener('click', function(){
+            const promoInput = document.getElementById('remove-outline')
+            const promoInputValue = promoInput.value
+            if(promoInputValue === 'SELL200'){
+                document.getElementById('apply-btn').addEventListener('click', function(){
+                    const discountValue = currentTotal * 0.2
+                    const discount = discountValue.toFixed(2)
+                    const totalValue = currentTotal - discount
+                    const total = totalValue.toFixed(2)
+                    document.getElementById('discount').innerText = discount
+                    document.getElementById('total').innerText = total
+                })
+            }
+        })
+        document.getElementById('apply-btn').disabled = false
+        document.getElementById('apply-btn').style.backgroundColor = '#E527B2'
+    }
+
+    const discount = 0
+    const totalValue = currentTotal - discount
+    const total = totalValue.toFixed(2)
+    document.getElementById('discount').innerText = discount
+    document.getElementById('total').innerText = total
+
+    if(total > 0){
+        document.getElementById('purchase-btn').disabled = false
+        document.getElementById('purchase-btn').style.backgroundColor = '#E527B2'
+    }
+})
+document.getElementById('prod6').addEventListener('click', function(){
+    const prodPriceValue = document.getElementById('quantity1')
+    const prodPriceString = prodPriceValue.innerText
+    const prodPrice = parseFloat(prodPriceString)
+
+    const totalElement = document.getElementById('total-price')
+    const previousTotalString = totalElement.innerText
+    const previousTotal = parseFloat(previousTotalString)
+
+    const currentTotalQuantity = previousTotal + prodPrice
+    const currentTotal = currentTotalQuantity.toFixed(2)
+
+    totalElement.innerText = currentTotal
+
+    if(currentTotal >= 200){
+        document.getElementById('apply-btn').addEventListener('click', function(){
+            const promoInput = document.getElementById('remove-outline')
+            const promoInputValue = promoInput.value
+            if(promoInputValue === 'SELL200'){
+                document.getElementById('apply-btn').addEventListener('click', function(){
+                    const discountValue = currentTotal * 0.2
+                    const discount = discountValue.toFixed(2)
+                    const totalValue = currentTotal - discount
+                    const total = totalValue.toFixed(2)
+                    document.getElementById('discount').innerText = discount
+                    document.getElementById('total').innerText = total
+                })
+            }
+        })
+        document.getElementById('apply-btn').disabled = false
+        document.getElementById('apply-btn').style.backgroundColor = '#E527B2'
+    }
+
+    const discount = 0
+    const totalValue = currentTotal - discount
+    const total = totalValue.toFixed(2)
+    document.getElementById('discount').innerText = discount
+    document.getElementById('total').innerText = total
+
+    if(total > 0){
+        document.getElementById('purchase-btn').disabled = false
+        document.getElementById('purchase-btn').style.backgroundColor = '#E527B2'
+    }
+})
+document.getElementById('prod7').addEventListener('click', function(){
+    const prodPriceValue = document.getElementById('quantity1')
+    const prodPriceString = prodPriceValue.innerText
+    const prodPrice = parseFloat(prodPriceString)
+
+    const totalElement = document.getElementById('total-price')
+    const previousTotalString = totalElement.innerText
+    const previousTotal = parseFloat(previousTotalString)
+
+    const currentTotalQuantity = previousTotal + prodPrice
+    const currentTotal = currentTotalQuantity.toFixed(2)
+
+    totalElement.innerText = currentTotal
+
+    if(currentTotal >= 200){
+        document.getElementById('apply-btn').addEventListener('click', function(){
+            const promoInput = document.getElementById('remove-outline')
+            const promoInputValue = promoInput.value
+            if(promoInputValue === 'SELL200'){
+                document.getElementById('apply-btn').addEventListener('click', function(){
+                    const discountValue = currentTotal * 0.2
+                    const discount = discountValue.toFixed(2)
+                    const totalValue = currentTotal - discount
+                    const total = totalValue.toFixed(2)
+                    document.getElementById('discount').innerText = discount
+                    document.getElementById('total').innerText = total
+                })
+            }
+        })
+        document.getElementById('apply-btn').disabled = false
+        document.getElementById('apply-btn').style.backgroundColor = '#E527B2'
+    }
+
+    const discount = 0
+    const totalValue = currentTotal - discount
+    const total = totalValue.toFixed(2)
+    document.getElementById('discount').innerText = discount
+    document.getElementById('total').innerText = total
+
+    if(total > 0){
+        document.getElementById('purchase-btn').disabled = false
+        document.getElementById('purchase-btn').style.backgroundColor = '#E527B2'
+    }
+})
+document.getElementById('prod8').addEventListener('click', function(){
+    const prodPriceValue = document.getElementById('quantity1')
+    const prodPriceString = prodPriceValue.innerText
+    const prodPrice = parseFloat(prodPriceString)
+
+    const totalElement = document.getElementById('total-price')
+    const previousTotalString = totalElement.innerText
+    const previousTotal = parseFloat(previousTotalString)
+
+    const currentTotalQuantity = previousTotal + prodPrice
+    const currentTotal = currentTotalQuantity.toFixed(2)
+
+    totalElement.innerText = currentTotal
+
+    if(currentTotal >= 200){
+        document.getElementById('apply-btn').addEventListener('click', function(){
+            const promoInput = document.getElementById('remove-outline')
+            const promoInputValue = promoInput.value
+            if(promoInputValue === 'SELL200'){
+                document.getElementById('apply-btn').addEventListener('click', function(){
+                    const discountValue = currentTotal * 0.2
+                    const discount = discountValue.toFixed(2)
+                    const totalValue = currentTotal - discount
+                    const total = totalValue.toFixed(2)
+                    document.getElementById('discount').innerText = discount
+                    document.getElementById('total').innerText = total
+                })
+            }
+        })
+        document.getElementById('apply-btn').disabled = false
+        document.getElementById('apply-btn').style.backgroundColor = '#E527B2'
+    }
+
+    const discount = 0
+    const totalValue = currentTotal - discount
+    const total = totalValue.toFixed(2)
+    document.getElementById('discount').innerText = discount
+    document.getElementById('total').innerText = total
+
+    if(total > 0){
+        document.getElementById('purchase-btn').disabled = false
+        document.getElementById('purchase-btn').style.backgroundColor = '#E527B2'
+    }
+})
+document.getElementById('prod9').addEventListener('click', function(){
+    const prodPriceValue = document.getElementById('quantity1')
+    const prodPriceString = prodPriceValue.innerText
+    const prodPrice = parseFloat(prodPriceString)
+
+    const totalElement = document.getElementById('total-price')
+    const previousTotalString = totalElement.innerText
+    const previousTotal = parseFloat(previousTotalString)
+
+    const currentTotalQuantity = previousTotal + prodPrice
+    const currentTotal = currentTotalQuantity.toFixed(2)
+
+    totalElement.innerText = currentTotal
+
+    if(currentTotal >= 200){
+        document.getElementById('apply-btn').addEventListener('click', function(){
+            const promoInput = document.getElementById('remove-outline')
+            const promoInputValue = promoInput.value
+            if(promoInputValue === 'SELL200'){
+                document.getElementById('apply-btn').addEventListener('click', function(){
+                    const discountValue = currentTotal * 0.2
+                    const discount = discountValue.toFixed(2)
+                    const totalValue = currentTotal - discount
+                    const total = totalValue.toFixed(2)
+                    document.getElementById('discount').innerText = discount
+                    document.getElementById('total').innerText = total
+                })
+            }
+        })
+        document.getElementById('apply-btn').disabled = false
+        document.getElementById('apply-btn').style.backgroundColor = '#E527B2'
+    }
+
+    const discount = 0
+    const totalValue = currentTotal - discount
+    const total = totalValue.toFixed(2)
+    document.getElementById('discount').innerText = discount
+    document.getElementById('total').innerText = total
+
+    if(total > 0){
+        document.getElementById('purchase-btn').disabled = false
+        document.getElementById('purchase-btn').style.backgroundColor = '#E527B2'
+    }
+})
+
